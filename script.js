e4d208a1cb98c817d6d06d5f91f4c3531a537027
Gareth Hines <gareth_hines@hotmail.com>
2026-02-25 07:50:28 +0000

    Add playRound logic

--- script.js
+++ script.js
@@ -18,14 +18,34 @@ function getComputerChoice() {
 }
 
 function getHumanChoice() {
-    let humanChoice = prompt('Welcome to the game, do you choose Rock, paper, or scissors?').toLowerCase();
+    let humanChoice = prompt('Welcome to the game, do you choose Rock, Paper, or Scissors?').toLowerCase();
     return humanChoice;
 }
 
 function playRound(humanChoice, computerChoice){
-
+    if (humanChoice === computerChoice) {
+        console.log(`You both chose ${humanChoice}, it's a draw!`)
+    } else if (humanChoice === 'rock' && computerChoice === 'paper') {
+        console.log(`You chose ${humanChoice}, computer chose ${computerChoice}, computer wins!`)
+        computerScore += 1;
+    } else if (humanChoice === 'rock' && computerChoice === 'scissors') {
+        console.log(`You chose ${humanChoice}, computer chose ${computerChoice}, you win!`)
+        humanScore += 1;
+    } else if (humanChoice === 'paper' && computerChoice === 'scissors') {
+        console.log(`You chose ${humanChoice}, computer chose ${computerChoice}, computer wins!`)
+        computerScore += 1;
+    } else if (humanChoice === 'paper' && computerChoice === 'rock') {
+        console.log(`You chose ${humanChoice}, computer chose ${computerChoice}, you win!`)
+        humanScore += 1;
+    } else if (humanChoice === 'scissors' && computerChoice === 'rock') {
+        console.log(`You chose ${humanChoice}, computer chose ${computerChoice}, computer wins!`)
+        computerScore += 1;
+    } else if (humanChoice === 'scissors' && computerChoice === 'paper') {
+        console.log(`You chose ${humanChoice}, computer chose ${computerChoice}, you win!`)
+        humanScore += 1;}
 }
 
 console.log(getHumanChoice())
+playRound(getHumanChoice(), getComputerChoice())
 
 // console.log(getComputerChoice());
